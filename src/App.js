49b6be1881deedaa14e1282a95da6c81c54a0aff
chuvkin <89609011+chuvkin@users.noreply.github.com>
2022-06-07 15:09:44 -0400

    Adds Die Hard and rotation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-import { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
+import { useRef, useState, Suspense } from 'react'
+import * as THREE from 'three'
+import { Canvas, useFrame, useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+import img from '../public/dieHard.jpg'
+
+// function Shelf(props) {
+//   const gltf = useLoader(GLTFLoader, '/scene.gltf')
+//   return (
+//     <Suspense fallback={null}>
+//       <primitive object={gltf.scene} />
+//     </Suspense>
+//   )
+// }
 
 function Box(props) {
   // This reference gives us direct access to the THREE.Mesh object
@@ -9,9 +22,14 @@ function Box(props) {
   const [clicked, click] = useState(false)
   // Subscribe this component to the render-loop, rotate the mesh every frame
   useFrame((state, delta) => {
-    ref.current.rotation.y += 0.0
-    ref.current.rotation.x -= 0.0
+    if (hover) {
+      ref.current.rotation.y += 0.01
+      ref.current.rotation.x -= 0.0
+    }
   })
+
+  const texture = useLoader(THREE.TextureLoader, img)
+
   // Return the view, these are regular Threejs elements expressed in JSX
   return (
     <mesh
@@ -21,8 +39,9 @@ function Box(props) {
       onClick={(event) => click(!clicked)}
       onPointerOver={(event) => hover(true)}
       onPointerOut={(event) => hover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
+      <boxGeometry args={[1, 1.5, 0.25]} />
       <meshStandardMaterial color={hovered ? 'hotpink' : 'blue'} />
+      <meshBasicMaterial attach="material" map={texture} />
     </mesh>
   )
 }
